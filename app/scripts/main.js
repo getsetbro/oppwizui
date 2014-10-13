3279faa1375de51af5962e95b869a370a0714685
Seth Broweleit <hello[@]getsetbro[.]com>
2014-10-13 11:58:57 -0500

    v2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,18 +9,18 @@
     var svg = d3.select('svg').attr({
         //'xmlns':'http://www.w3.org/2000/svg',
         //'xlink:xlink':'http://www.w3.org/1999/xlink',
-        'viewBox': -(distBetween/2) + ' 0 1200 45',
+        'viewBox': -(distBetween / 2) + ' 0 1200 45',
         //'preserveAspectRatio':'xMinYMin',
         //'preserveAspectRatio':'xMidYMin',
         //'preserveAspectRatio':'xMaxYMin',
         //'preserveAspectRatio':'xMinYMid',
-        'preserveAspectRatio':'xMidYMid meet',
+        'preserveAspectRatio': 'xMidYMid meet',
         //preserveAspectRatio:"xMidYMid slice",
         //'preserveAspectRatio':'xMaxYMid',
         //'preserveAspectRatio': 'xMinYMax',
         //'preserveAspectRatio':'xMidYMax',
         //'preserveAspectRatio':'xMaxYMax',
-        'height': 143,
+        'height': 130,
         'width': '100%'
     });
 
@@ -28,7 +28,7 @@
     var circles = svg.selectAll('z')
         .data(data)
         .enter();
-
+//lines
     circles.append('line')
         .attr({
             x1: function(d, i) {
@@ -48,27 +48,28 @@
             },
             'stroke-width': 5
         });
-
+//big outer circles
     circles.append('circle')
         .attr({
             cx: function(d, i) {
                 return (distBetween * i);
             },
             cy: 0,
-            r: 22,
+            r: 20,
             fill: function(d, i) {
                 var color = (progress <= i) ? '#6D7174' : '#7EA7D3';
                 return color;
             }
         });
 
+//smaller inner circles
     circles.append('circle')
         .attr({
             cx: function(d, i) {
                 return (distBetween * i);
             },
             cy: 0,
-            r: 16,
+            r: 12,
             fill: function(d, i) {
                 var color = (progress <= i) ? '#afafb0' : '#D2E2E5';
                 return color;
@@ -77,6 +78,7 @@
             'stroke-width': 2
         });
 
+//black circle
     circles.append('circle')
         .attr({
             cx: function(d, i) {
@@ -84,12 +86,11 @@
             },
             cy: 0,
             r: function(d, i) {
-                var prog = (progress === (i + 1)) ? 10 : 0;
+                var prog = (progress === (i + 1)) ? 8 : 0;
                 return prog;
             },
             fill: '#464646'
         });
-
 
     circles.append('text')
         .attr({
